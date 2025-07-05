6eb2a4fd693b1a3b2f901f5a1ef80be942f06b77
FireatomYT <vovakorablin51@gmail.com>
2025-07-05 14:06:53 +0300

    Update linux-start.sh

--- linux-start.sh
+++ linux-start.sh
@@ -2,7 +2,7 @@
 
 welcome() {
     echo ""
-    echo "Welcome to Fabi's Linux launcher"
+    echo "Welcome to FABI's Linux launcher"
     echo "Please select an option to begin:"
     echo ""
     echo "   1. Start the bot normally"
@@ -40,7 +40,7 @@ startLoop() {
 }
 
 startApplication() {
-    java -jar Main.jar
+    java -jar FABI.jar
 
     if [ "$1" != "false" ]; then
         welcome
